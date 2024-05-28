0fda0b8b51e310bc758395af17708206c84b7b3c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-28 12:04:21 -0300

    Ac1 AyumiFlix

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import data from '../../articles.json'
+
+function Home() {
+    console.log(data)
+    return ( 
+        <main>
+            <input type="text" name="busca" id="buscar" placeholder="Digite aqui sua busca"/>
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    
+                data.map(filme => (
+                    <div className='card bg-ayumi' key={filme.title}>
+                    <h1>{filme.title}</h1>
+                    <img src={filme.image} alt={filme.title} />
+
+                    <div className='texto'>
+                        {filme.text.map(texto => (
+                            <p key={texto}>{texto}</p>
+                        ))}
+                    </div>
+                    
+                    <div className='tag'>
+                        {filme.tags.map(tag => (
+                            <span key={tag} className='text-blue-700'> {tag} </span>
+                        ))}
+                    </div>
+                    </div>
+                ))}
+            </div>
+        </main>
+     );
+}
+
+export default Home ;
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-ayu': '#1552AA',
+        'ayumi': '#779ACE'
+      }
+    },
+  },
+  plugins: [],
+}
